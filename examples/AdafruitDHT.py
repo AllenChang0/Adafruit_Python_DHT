6d3f09d2e279a91a2055d109368bbb6e33b52e0c
AllenChang0 <chuangwen1836@gmail.com>
2019-11-27 12:12:40 +0800

    a

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -19,7 +19,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import RPi.GPIO as GPIO
+
 import sys
 import Adafruit_DHT
 import time
@@ -28,8 +28,8 @@ import urllib
 import json
 deviceId = "D0SCVGKp"
 deviceKey = "0dWTwycBUZAxGaI4"
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
 def post_to_mcs(payload):
 	headers = {"Content-type":"application/json","deviceKey":deviceKey}
 	not_connected = 1
@@ -63,7 +63,6 @@ else:
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
 while True:
 	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-	SwitchStatus = GPIO.input(24)
 # Un-comment the line below to convert the temperature to Fahrenheit.
 # temperature = temperature * 9/5.0 + 32
 
@@ -81,11 +80,4 @@ while True:
 		print('Failed to get reading. Try again!')
 		sys.exit(1)
 
-	if(SwitchStatus == 0):
-		print('Button pressed')
-		payload={"datapoints":[{"dataChnId":"Humidity","values":{"value":humidity}},{"dataChnId":"Temperature","values":{"value":temperature}},{"dataChnId":"SwitchStatus","values":{"value":SwitchStatus}}]}
-		post_to_mcs(payload)
-	else:
-		print('Button released')
-		payload={"datapoints":[{"dataChnId":"Humidity","values":{"value":humidity}},{"dataChnId":"Temperature","values":{"value":temperature}},{"dataChnId":"SwitchStatus","values":{"value":SwitchStatus}}]}
-		post_to_mcs(payload)
+
